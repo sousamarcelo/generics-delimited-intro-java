9638d38760266d657652123b104d0b591ed1e706
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2022-01-08 21:49:28 -0300

    Create pakage 'model.service' --> class CalculationService

---
+++ src/model/service/CalculationService.java
@@ -0,0 +1,21 @@
+package model.service;
+
+import java.util.List;
+
+public class CalculationService {
+	
+	public static <T extends Comparable<T>> T max(List<T> list) {
+		if (list.isEmpty()) {
+			throw new IllegalThreadStateException("List cant´s be empty");
+		}
+		
+		T max = list.get(0);
+		for(T item : list) {
+			if (item.compareTo(max) > 0) {
+				max = item;
+			}
+		}
+		return max;
+	}
+
+}
